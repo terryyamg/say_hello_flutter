902855dccd56527631e8f4eb0abea8d9ea8033f6
terry yang <terryyamg@gmail.com>
2021-11-17 10:41:04 +0800

    開始啦!!! Flutter!!! 先Commit一波~ 讚讚

---
+++ README.md
@@ -0,0 +1,9 @@
+# say_hello_flutter
+
+Flutter practise project.
+
+## Getting Started
+
+第一次寫Flutter就上手
+主要架構: provider + MVVM 
+套件: 慢慢加
---
+++ android/app/src/main/kotlin/com/terryyamg/say_hello_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.terryyamg.say_hello_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
